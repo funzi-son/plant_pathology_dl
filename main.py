21a87a445340b0d9aff523977d49d4c2a663f6f6
Jianping Yao <Jianping.Yao@utas.edu.au>
2022-11-07 13:06:27 +1100

    07Nov2022_by_Frank_v1

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
+'''
+Plant Pathology Deep Learning
 
-# dataset_dir = '/home/jyao1/Leaf_diseases/Datasets/'
-# save_path = '/home/jyao1/Leaf_diseases/test_model/'
-dataset_dir = 'D:/IT_learning/ResearchMaster/ComputerVision/Data/'
-save_path = 'D:/IT_learning/test_model/'
+This is the code for paper "Deep Learning for Plant Identification and Disease Classification
+from Leaf Images: Multi-prediction Approaches".
+Please modify the parameters at here, then run main.py.
+'''
 
-# TF_weights='imagenet' #
+dataset_dir = '/home/user/Leaf_diseases/Datasets/'  # please change the path of the datasets. 
+save_path = '/home/user/Leaf_diseases/test_model/'    # please change the path of results and model will be saved.
+
+# TF_weights='imagenet'
 TF_weights=None
 
-fig_size = 32 #256
-INIT_LR = 0.001    # INIT_LR = 0.004 
+fig_size = 256
+INIT_LR = 0.001
 op_z = "Adamax"
 
 # obj = "multi_model"
@@ -27,20 +32,22 @@ saveornot = 'save'
 # saveornot = 'not'
 
 bat_si = 16   # batch_size
-epo = 2 #10000 # epochs
-times = 1#0
+epo = 10000 # epochs
+times = 10
 
 # model_name =  'CNN'
 # model_name ='AlexNet'
-#'VGG', 'ResNet'
-# model_name =      'EfficientNet' 
-# model_name =    'Inception'
-model_name =   'MobileNet'
+# model_name = 'VGG'
+# model_name = 'ResNet'
+# model_name = 'EfficientNet' 
+model_name = 'Inception'
+# model_name =   'MobileNet'
 
 p_t_w = 0.1
 d_t_w = 0.1
 p_w = 0.4
 d_w = 0.5   
+
 balance_weight = [p_t_w,d_t_w,p_w,d_w]
 
 
